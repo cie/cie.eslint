21e3d4fb95abf7137ea174b80bfe137b89006ceb
Bernát Kalló <kallo.bernat@gmail.com>
2016-06-28 14:19:00 +0000

    added info level messages, added sample .eslintrc

--- worker/eslint_worker.js
+++ worker/eslint_worker.js
@@ -8,6 +8,7 @@ define(function(require, exports, module) {
     };
 
     var path = require("plugins/c9.ide.language.javascript.infer/path");
+    var working = false;
     
     handler.analyze = function(docValue, ast, callback) {
         var basePath = path.getBasePath(handler.path, handler.workspaceDir);
@@ -22,17 +23,23 @@ define(function(require, exports, module) {
             if (finished) return;
             timeouted = true;
             callback([{ pos: { sl: 0 }, message: "Eslint took too long to analyze.", level: "warning" }]);
-        }, 3000)
+        }, 4500)
         
         var cmd = "echo \""+docValue.replace(/([\\"$`])/g, "\\$1")+"\" | eslint_d --parser=babel-eslint --stdin --format=json --stdin-filename " + filePath.replace(/([^a-zA-Z0-9_\/~.-])/g, "\\$1")
+        if (working) {
+            callback([{ pos: { sl: 0 }, message: "Eslint is still initializing...", level: "warning" }]);
+            return;
+        }
+        working = true;
         workerUtil.execFile(
             "bash",
             {
                 args: ["-c", cmd],
                 maxCallInterval: 50,
-                timeout: 4000,
+                timeout: 20000,
             },
             function(err, stdout, stderr) {
+                working = false;
                 if (timeouted) return;
                 finished = true;
                 if (err && err.code !== 255 && err.code !== 1 && err.code !== 2) return callback([{pos: {sl: 0}, message:e.message, level: "error"}]);
@@ -45,10 +52,13 @@ define(function(require, exports, module) {
                 var markers = [];
                 stdout.forEach(function(file) {
                     file.messages.forEach(function(m) {
+                        const info = m.message.match(/too many|maximum line length|semicolon|multiple spaces|'else'/i)
+                        const level = info ? 'info' : (m.severity === 1 ? "warning" : "error")
                         markers.push({
                             pos: { sl: m.line - 1, sc: m.column - 1, ec: m.column - 1 + m.source.length },
                             message: m.message,
-                            level: m.severity === 1 ? "warning" : "error",
+                            type: level,
+                            level: level,
                         })
                     })
                 });
